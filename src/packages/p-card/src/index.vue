d73da20199cfff6da9ef460dbac0f8592cebcdd1
ZhangPQ <>
2022-11-18 09:32:36 +0800

    存档

--- src/packages/p-card/src/index.vue
+++ src/packages/p-card/src/index.vue
@@ -1,49 +1,43 @@
 <template>
-  <p-link
-      :class="['p-card',type]"
-      :href="href"
-      :target="target"
-      style="text-decoration:none"
-  >
+  <p-link :class="['p-card', type]" :href="href" :target="target" style="text-decoration: none">
     <slot></slot>
-    <div class="card-img"  v-if="url" :style="url"></div>
+    <div class="card-img" v-if="url" :style="url"></div>
     <div class="p-card-info">
-      <h4 class="detail-title" v-if="title">{{title}}</h4>
-      <p class="detail-describe" v-if="describe">{{describe}}</p>
-      <p class="detail-label" v-if="label">{{label}}</p>
+      <h4 class="detail-title" v-if="title">{{ title }}</h4>
+      <p class="detail-describe" v-if="describe">{{ describe }}</p>
+      <p class="detail-label" v-if="label">{{ label }}</p>
     </div>
-
   </p-link>
 </template>
 
 <script>
 export default {
-	name: "p-card",
-	props: {
-		href: String,
-		img: Object,
-		title: String,
-		label: String,
-		url: Object,
-		describe: String,
-		target: String,
-	},
+  name: 'p-card',
+  props: {
+    href: String,
+    img: Object,
+    title: String,
+    label: String,
+    url: Object,
+    describe: String,
+    target: String,
+  },
 };
 </script>
 
 <style scoped="scoped">
-.p-card{
+.p-card {
   position: relative;
   width: 100%;
   height: 100%;
   border-radius: 16px;
-  background-color: #F5F5F7;
+  background-color: #f5f5f7;
 }
 .p-card .card-img {
-  border-radius: 16px 16px 0 0 ;
+  border-radius: 16px 16px 0 0;
   height: 65%;
   /*box-shadow: 0 5px 10px 0 #e0e0e0;*/
-  background-color:#cccccc;
+  background-color: #cccccc;
 }
 .p-card .p-card-info {
   position: relative;
@@ -54,31 +48,31 @@ export default {
   bottom: 0px;
   height: 30%;
 }
-.p-card .p-card-info .detail-title{
+.p-card .p-card-info .detail-title {
   position: relative;
   font-size: 20px;
   font-weight: 600;
   color: #403f3f;
-  text-align:left;
+  text-align: left;
   padding-left: 16px;
   margin: 0;
   /*height: 100%;*/
   /*padding: 0px;*/
 }
-.p-card .p-card-info .detail-describe{
+.p-card .p-card-info .detail-describe {
   position: relative;
   font-size: 14px;
   font-weight: 400;
   color: #403f3f;
   padding: 10px 20px 10px 16px;
-  text-align:left;
+  text-align: left;
   margin: 0;
   display: block;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 }
-.p-card .p-card-info .detail-label{
+.p-card .p-card-info .detail-label {
   position: absolute;
   font-size: 14px;
   font-weight: 400;
@@ -89,18 +83,17 @@ export default {
   text-align: right;
 }
 .p-head-link {
-  text-decoration:none;
-  display:block;
+  text-decoration: none;
+  display: block;
   font-size: 16px;
-  color: #1D1D1F;
-  margin: 18px auto ;
+  color: #1d1d1f;
+  margin: 18px auto;
   line-height: 30px;
 }
 .p-head-link-img {
-  display:block;
-  color: #1D1D1F;
-  text-decoration:none;
-  margin: 16px auto ;
+  display: block;
+  color: #1d1d1f;
+  text-decoration: none;
+  margin: 16px auto;
 }
-
 </style>
